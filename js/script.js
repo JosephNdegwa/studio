25fc9ea82ff35257321ee0b62acd7e8c38b901d0
JosephNdegwa <kariukindegwa98@gmail.com>
2020-10-13 01:06:42 +0300

    update form script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
   $('.frame').hover(function(){
-    $('.caption',this).slideToggle('slow');
-  }, function() {
-    $('.caption',this).slideToggle('slow');
+  $('.caption',this).slideToggle('slow');
+}, function(){
+  $('.caption',this).slideToggle('slow');
   });
+});
+
+$(document).ready(function(){
+  $("form#formed").submit(function(event){
+    var name = $("input#named").val();
+    var email = $("input#mail").val();
+    var message = $("textarea#message").val();
+    if ($("input#named").val() && $("input#mail").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+    
+  });
+
 });
